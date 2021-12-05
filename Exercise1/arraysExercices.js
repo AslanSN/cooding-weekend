fb17b58d760fba1d7db255db10a647bb4525fae0
AslanSN <alan.staub@hotmail.com>
2021-12-05 13:32:09 +0100

    Npm

--- Exercise1/arraysExercices.js
+++ Exercise1/arraysExercices.js
@@ -1,17 +1,18 @@
 /*
-NORMAS
-crear una funcion que genere una escusa aleatoria con esos datos 
-creeis otra funcion que cuente el numero de repeticiones de letras en cada array
-suprimir repeticiones en un array y devolver el array sin la repeticion
+TAREAS
+1. crear una funcion que genere una excusa aleatoria con esos datos 
+2. creeis otra funcion que cuente el numero de repeticiones de letras en cada array
+3. suprimir repeticiones en un array y devolver el array sin la repeticion
 function que invierta todos los valores de el array
-normas
-documentado
-prohibido la programacion declarativa
-debe estar separado en funciones
-no se pueden utilizar objetos
-obligatorio programacion funcional
-todas las variables y nombres en funcoines en ingles
-escritura camel-case
+
+NORMAS
+- documentado
+- prohibido la programacion declarativa
+- debe estar separado en funciones
+- no se pueden utilizar objetos
+- obligatorio programacion funcional
+- todas las variables y nombres en funcoines en ingles
+- escritura camel-case
 */
 
 // datos iniciales 
